8c92585fd324f9cecfbc0fdd526c58d0f7930634
aplet <antoine.plet@neuf.fr>
2011-11-07 17:38:49 +0100

    test affichage

--- scripts/app.js
+++ scripts/app.js
@@ -108,7 +108,7 @@ window.fbAsyncInit = function() {
 		var limite = 10;
 		var modifie = 1;
 //		while(modifie == 1)
-		for(var ind = 0 ; ind < 0 ; ind++)
+		for(var ind = 0 ; ind < 1 ; ind++)
 		{
 			modifie = 0;
 			for(id1 in graphe)
