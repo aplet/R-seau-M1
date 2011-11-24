6b193fa3801b7516597b173770546c67f2a8bf9e
aplet <antoine.plet@neuf.fr>
2011-11-24 17:00:41 +0100

    nombre d'itération modifié

--- scripts/app.js
+++ scripts/app.js
@@ -195,9 +195,9 @@ window.fbAsyncInit = function() {
 		{
 			canvas.circle(graphe[id]["pos_x"], graphe[id]["pos_y"], rayon)
 				.attr({fill: "blue"})
+				.mouseover(affichage);
 				.id = id
 //				.data("uid", graphe[id])
-//				.mouseover(affichage);
 //			$('#friends').append('<div>' + id + " --> (" + graphe[id]["pos_x"] + ", " + graphe[id]["pos_y"] + ")\n" + '</div>');
 		}
 
